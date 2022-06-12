71e60a8cbe8985d9ac4077a11f293bc9c94ab3d4
parishkar-nagrikar <p4parish@gmail.com>
2022-06-12 23:54:14 +0530

    first commit

---
+++ src/main/java/com/shopoedia/discovey/DiscoveyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopoedia.discovey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopoedia/discovey/DiscoveyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopoedia.discovey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
